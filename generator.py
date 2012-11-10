afb9cd4d8eeb9e7197df1878637603d1680774d9
Andres F. Cardenas <andres.cardenas@axiacore.com>
2012-11-10 13:30:21 -0500

    Added minor changes, and added structure for problem

---
+++ generator.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Andres F. Cardenas (Andrewnix)
+# Email: akardenasjimenez@gmail.com
+
+class SQLGenerator:
+    def select(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
---
+++ tests.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Andres F. Cardenas (Andrewnix)
+# Email: akardenasjimenez@gmail.com
